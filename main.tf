76b231d246950876d2f82fa60c3bd33e79f20001
Rajesh <savalamrajesh8142@gmail.com>
2025-08-02 15:18:58 +0530

    azure vnet

--- main.tf
+++ main.tf
@@ -10,3 +10,27 @@ resource "azurerm_virtual_network" "edge" {
 
   depends_on = [azurerm_resource_group.base]
 }
+resource "azurerm_subnet" "rest" {
+  name                 = "rest"
+  resource_group_name  = azurerm_resource_group.base.name
+  virtual_network_name = azurerm_virtual_network.edge.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_subnet" "cnt" {
+  name                 = "cnt"
+  resource_group_name  = azurerm_resource_group.base.name
+  virtual_network_name = azurerm_virtual_network.edge.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_subnet" "plo" {
+  name                 = "plo"
+  resource_group_name  = azurerm_resource_group.base.name
+  virtual_network_name = azurerm_virtual_network.edge.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+resource "azurerm_subnet" "ner" {
+  name                 = "ner"
+  resource_group_name  = azurerm_resource_group.base.name
+  virtual_network_name = azurerm_virtual_network.edge.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
